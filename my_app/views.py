1fcb26114adc6d5fa11cdf1ce473120d8d349d13
DesTu <maurycy-vip@wp.pl>
2025-02-08 13:36:48 +0100

    Templates

--- my_app/views.py
+++ my_app/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+
 from django.http import HttpResponse
 
+# programming = ("Python", "JavaScript", "Java", "C#")
+
+
+# def home(request):
+#     return HttpResponse("Welcome Home")
 
 def home(request):
-    return HttpResponse("Welcome Home")
+    return render(request, 'my_app/home.html')
